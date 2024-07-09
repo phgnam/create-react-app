94e99be741671679459bb56615ffe56bb9d9b31d
Phuong Nam <09749388xx@gmail.com>
2024-07-10 00:10:52 +0700

    Memorized Handler in React (Advanced)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -22,26 +22,27 @@ const storiesReducer = (state, action) => {
         ...state,
         isLoading: true,
         isError: false,
-      }
+      };
     case "STORIES_FETCH_SUCCESS":
       return {
         ...state,
         isLoading: false,
         isError: false,
-        data: action.payload
-      }
+        data: action.payload,
+      };
     case "STORIES_FETCH_FAILURE":
       return {
         ...state,
         isLoading: false,
         isError: true,
-      }
+      };
     case "REMOVE_STORY":
       return {
         ...state,
         data: state.filter(
-        (story) => action.payload.objectID !== story.objectID
-      )};
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
     default:
       throw new Error();
   }
@@ -56,22 +57,26 @@ const App = () => {
     isError: false,
   });
 
-  React.useEffect(() => {
-    if (searchTerm === '') return;
+  const handleFetchStories = React.useCallback(() => {
+    if (!searchTerm) return;
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
     fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload:result.hits,
+          type: "STORIES_FETCH_SUCCESS",
+          payload: result.hits,
         });
       })
       .catch(() => {
         dispatchStories({ type: "STORIES_FETCH_FAILURE" });
       });
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
