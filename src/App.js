5c4bbbb0486895364f1695c1136d3612181620c7
Phuong Nam <09749388xx@gmail.com>
2024-07-10 00:04:42 +0700

    Data Re-Fetching in React

--- src/App.js
+++ src/App.js
@@ -57,8 +57,10 @@ const App = () => {
   });
 
   React.useEffect(() => {
+    if (searchTerm === '') return;
+
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -69,7 +71,7 @@ const App = () => {
       .catch(() => {
         dispatchStories({ type: "STORIES_FETCH_FAILURE" });
       });
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -81,10 +83,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div>
@@ -105,7 +103,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
